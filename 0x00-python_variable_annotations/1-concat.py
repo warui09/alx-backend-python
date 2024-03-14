bf1e013ef90e73d3bdc94bbca337ce18960d12d5
Kevin Warui <waruimacharia@gmail.com>
2024-03-14 11:17:51 +0000

    Edit documentation on 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
 """
 Module to perform concat operations.
-
-Functions:
-    concat(str1: str, str2: str) -> str: concats two strings and returns a string.
 """
 
 
 def concat(str1: str, str2: str) -> str:
     """
     Concatenate two trings and return the result.
-
-    Args:
-        str1 (str): The first string.
-        str2 (str): The second string.
-
-    Returns:
-        string: The result of concatenating str2 to str1.
     """
 
     return str1 + str2
