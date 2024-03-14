a69cb6c6315c49b76ed2c10a51af971842d90b63
Kevin Warui <waruimacharia@gmail.com>
2024-03-14 12:33:30 +0000

    Add 7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Takes a string and an int or float and returns a tuple
+"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Returns a tuple of k and v
+    """
+
+    return (k, v)
