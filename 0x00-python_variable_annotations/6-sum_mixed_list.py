69d67724f290f4caaa3d40be318533f1ce6c8b31
Kevin Warui <waruimacharia@gmail.com>
2024-03-14 12:08:33 +0000

    Add 6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Calculate sum of floats and integers
+"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """
+    Return the sum of the elements of the list
+    """
+
+    return sum(mxd_lst)
