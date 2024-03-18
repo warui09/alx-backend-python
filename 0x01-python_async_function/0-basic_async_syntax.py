64e758472fe40f35f0051cb1b744464e885650c7
Kevin Warui <waruimacharia@gmail.com>
2024-03-18 10:14:39 +0000

    Add 2-measure_runtime.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-'''this module contains the solution to task 0'''
+"""this module contains the solution to task 0"""
 
 import asyncio
 import random
 
 async def wait_random(max_delay: int = 10) -> float:
-    '''waits for a random delay between 0 and max_delay
-    (included and float value) seconds and eventually returns it'''
+    """waits a random amoint of seconds and then returns the seconds"""
 
     random_float = random.uniform(0, max_delay)
     await asyncio.sleep(random_float)
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -4,12 +4,12 @@
 import asyncio, time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """measures the total execution time for wait_n(n, max_delay),
     and returns total_time / n"""
 
     start = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end = time.time()
 
     return (end - start) / n
