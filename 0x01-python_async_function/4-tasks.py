c2343de4ea76236225424ab09e7a3dd5f43d00d1
Kevin Warui <waruimacharia@gmail.com>
2024-03-19 08:04:08 +0000

    Add function comment to 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,4 +8,6 @@ task_wait_random = __import__("3-tasks").task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """call task_wait_random n times"""
+
     return sorted([await task_wait_random(max_delay) for i in range(n)])
