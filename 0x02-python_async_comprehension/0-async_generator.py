74c05ae8fb2d63a08bc84147477431525967f27f
Kevin Warui <waruimacharia@gmail.com>
2024-03-19 09:15:31 +0000

    Change Generator return type from int to float

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ from typing import Generator
 import random
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """wait 1 second, then yield a random number between 0 and 10"""
 
     for i in range(10):
