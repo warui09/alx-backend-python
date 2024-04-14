3a2f12c078883a1ca27a621bcf73b6963833c845
Kevin Warui <waruimacharia@gmail.com>
2024-04-14 11:17:08 +0000

    Add test_public_repos_url to test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """task 4 solution"""
 
+from client import GithubOrgClient
 from parameterized import parameterized
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -15,7 +16,21 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_org(self, org, expected):
         """get_json once with the expected argument"""
 
-        with patch("client.GithubOrgClient.org") as mock_get:
-            mock_get(org).return_value = expected
-            mock_get.assert_called_once_with(org)
+        with patch("client.GithubOrgClient.org") as mock_org:
+            mock_org(org).return_value = expected
+            mock_org.assert_called_once_with(org)
             self.assertEqual(expected, {"login": "google"})
+
+    def test_public_repos_url(self):
+        """test GithubOrgClient._public_repos_url"""
+
+        with patch(
+                "client.GithubOrgClient.org", new_callable=PropertyMock
+            ) as mock_org:
+            mock_org.return_value = {
+                "repos_url": "https://api.github.com/users/google/repos"
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
